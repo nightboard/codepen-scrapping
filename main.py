b995d58c6f7dd644956a48a50a02f2a4ce160b1a
visrut <visrutnavadiya111@gmail.com>
2021-11-16 15:12:17 +0530

    docs functionality added

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.proxy import Proxy, ProxyType
 
+from docx import Document
+
 from codepen import get_codepen_data
 
 DATA_FILE = "links.txt"
-OUTPUT_FILE = 'output.txt'
 
 options = webdriver.FirefoxOptions()
 options.headless = True
@@ -14,23 +15,39 @@ driver = webdriver.Firefox(options=options, executable_path="./geckodriver")
 # set loding time for website & send request
 driver.implicitly_wait(10)
 
-with open(OUTPUT_FILE, 'a') as write_file:
-    i = 1
-    with open(DATA_FILE) as read_file:
-        for url in read_file:
-            URL = url.replace('\n', '')
+# document configuration
+document = Document()
+style = document.styles['Normal']
+font = style.font
+font.name = 'Sans Serif'
+style.paragraph_format.line_spacing = 0.5
 
-            codepen_data = get_codepen_data(driver, URL)
 
-            write_file.write(f"{URL}\n")
-            write_file.write(f"Title : {codepen_data['title']}\n")
-            write_file.write(f"Author : {codepen_data['author-name']}\n")
-            write_file.write(f"Made with : {codepen_data['made-with']}\n")
-            write_file.write(
-                f"Created date : {codepen_data['created-date']}\n")
-            write_file.write("\n\n\n\n")
+def add_meta_data(document, property_name, property_value=""):
+    paragraph = document.add_paragraph('')
+    run = paragraph.add_run(property_name)
+    run.bold = True
+    paragraph.add_run(property_value)
 
-            print(f"{i} ✅")
-            i += 1
 
-    driver.close()
+i = 1
+with open(DATA_FILE) as read_file:
+    for url in read_file:
+        URL = url.replace('\n', '')
+
+        codepen_data = get_codepen_data(driver, URL)
+
+        add_meta_data(document, f"{i}. Title: ", codepen_data['title'])
+        add_meta_data(document, "Author: ", codepen_data['author-name'])
+        add_meta_data(document, "Created On: ", codepen_data['created-date'])
+        add_meta_data(document, "Made with: ", codepen_data['made-with'])
+        add_meta_data(document, "Responsive: ")
+        add_meta_data(document, "Compatible browsers: ")
+        add_meta_data(document, "Description: ")
+        add_meta_data(document, "Code source link: ", f"{URL}\n\n\n")
+
+        print(f"{i} ✅")
+        i += 1
+    document.save("demo.docx")
+
+driver.close()
